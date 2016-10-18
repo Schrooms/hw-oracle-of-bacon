ae6526080af99825278b8bfbc5d6e560164b2944
Jeroen van Heiningen <jeroen.v.heiningen@gmail.com>
2016-10-18 05:57:05 +0000

    implemented homework

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -20,11 +20,13 @@ class OracleOfBacon
   validate :from_does_not_equal_to
 
   def from_does_not_equal_to
-    # YOUR CODE HERE
+    errors.add(:to, "From cannot be the same as To") if  @to == @from
   end
 
-  def initialize(api_key='')
+  def initialize(api_key='38b99ce9ec87')
     # your code here
+    @api_key = api_key
+    @from = @to = 'Kevin Bacon'
   end
 
   def find_connections
@@ -37,13 +39,17 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      raise NetworkError.new e.message
     end
+    @response = Response.new xml
     # your code here: create the OracleOfBacon::Response object
   end
 
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+    parameters = "p=#{CGI.escape(@api_key)}&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?" + parameters
   end
       
   class Response
@@ -59,15 +65,37 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else
+        parse_unknown_response
       end
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
     end
+    def parse_graph_response
+      @type = :graph
+      actors = @doc.xpath('//actor').map { |e| e.text }
+      movies = @doc.xpath('//movie').map { |e| e.text }
+      @data = actors.zip(movies).flatten.compact
+    end
+    
+    
+    def parse_spellcheck_response 
+      @type = :spellcheck
+      @data = @doc.xpath("//match").map { |e| e.text }
+    end
+    
+    
+    def parse_unknown_response 
+      @type = :unknown
+      @data = "unknown response type"
+    end
+    
   end
 end
 
